a4d1b374cb705fc598536823018fcadf50ef7b51
seungyeonk <88223759+seungyeonk@users.noreply.github.com>
2022-11-02 03:05:35 +0900

    hw3

--- HW3/IMDBStudent20200940.py
+++ HW3/IMDBStudent20200940.py
@@ -1,19 +1,21 @@
 import sys
+
 input_file = sys.argv[1]
 output_file = sys.argv[2]
-d = dict()
+
+dict = dict()
 fp = open(input_file)
 for line in fp:
     line = line.strip()
-    g = line.split('::')[2]
-    genres = g.split('|')
+    genre = line.split('::')[2]
+    genres = genre.split('|')
     for genre in genres :
-        if genre in d:
-            d[genre] = d[genre] + 1
+        if genre in dict:
+            dict[genre] = dict[genre] + 1
         else:
-            d[genre] = 1
+            dict[genre] = 1
 fp.close()
 f = open(output_file, 'w')
-for genre, count in d.items():
+for genre, count in dict.items():
     f.write(genre + ' ' + str(count) + '\n')
 f.close()
--- HW3/UBERStudent20200940.py
+++ HW3/UBERStudent20200940.py
@@ -1,27 +1,30 @@
 from datetime import datetime
 import sys
+
 input_file = sys.argv[1]
 output_file = sys.argv[2]
-days = ['MON', 'TUE', 'WED', 'THR', 'FRI', 'SAT', 'SUN']
-d = dict()
+
+day = ['MON', 'TUE', 'WED', 'THR', 'FRI', 'SAT', 'SUN']
+dict = dict()
 fp = open(input_file)
 for line in fp :
     line = line.strip()
     data = line.split(',')
     date = datetime.strptime(data[1], '%m/%d/%Y')
-    data[1] = days[date.weekday()]
+    data[1] = day[date.weekday()]
     data[2] = int(data[2])
     data[3] = int(data[3])
-    uberKey = (data[0], data[1])
-    uberValue = [data[2], data[3]]
-    if uberKey in d:
-        d[uberKey][0] += data[2]
-        d[uberKey][1] += data[3]
+    key = (data[0], data[1])
+    value = [data[2], data[3]]
+
+    if key in dict:
+        dict[key][0] += data[2]
+        dict[key][1] += data[3]
     else:
-        d[uberKey] = uberValue
+        dict[key] = value
 fp.close()
 f = open(output_file, 'w')
-for key, count in d.items():
+for key, cnt in dict.items():
     f.write(key[0] + "," + key[1])
-    f.write(" %d,%d\n" % (count[0], count[1]))
+    f.write(" %d,%d\n" % (cnt[0], cnt[1]))
 f.close()
